0fd7de45b42832543990e4f1d8aa2739f01e81d3
AhmedAbogameel <ahmedabogameel81@gmail.com>
2023-08-29 19:48:37 +0300

    fix validator middleware bug

--- middlewares/validator_middleware.js
+++ middlewares/validator_middleware.js
@@ -2,11 +2,11 @@ const { validationResult } = require('express-validator');
 
 // @desc  Finds the validation errors in this request and wraps them in an object with handy functions
 const validatorMiddleware = (req, res, next) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ message: errors.array().at(0).msg });
-    }
-    next();
-  };
-  
-  module.exports = validatorMiddleware;
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ message: errors.array()[0].msg });
+  }
+  next();
+};
+
+module.exports = validatorMiddleware;
